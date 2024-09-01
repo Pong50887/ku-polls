5fe86c4276268ee269963fb66cbedfe32883855d
Pong <115053913+Pong50887@users.noreply.github.com>
2024-09-01 17:48:37 +0700

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/polls/', permanent=True))
+    path('', RedirectView.as_view(pattern_name='polls:index', permanent=True))
 ]

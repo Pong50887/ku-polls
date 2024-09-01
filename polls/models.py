423882a8c17b610581a021682225428fbc39273b
Pong <115053913+Pong50887@users.noreply.github.com>
2024-09-01 17:15:18 +0700

    update Question model

--- polls/models.py
+++ polls/models.py
@@ -2,22 +2,44 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    end_date = models.DateTimeField('date ended', null=True, blank=True)
+
+    def __str__(self):
+        return self.question_text
 
     @admin.display(
         boolean=True,
         ordering='pub_date',
         description='Published recently?',
     )
-
-    def __str__(self):
-        return self.question_text
-
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Is published?',
+    )
+    def is_published(self):
+        now = timezone.localtime()
+        return now >= self.pub_date
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Can vote?',
+    )
+    def can_vote(self):
+        now = timezone.localtime()
+        if self.end_date:
+            return self.pub_date <= now <= self.end_date
+        return now >= self.pub_date
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)

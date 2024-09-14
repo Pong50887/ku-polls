08d11de4f56779a17d6d034b67ec281c77de0456
Pong <115053913+Pong50887@users.noreply.github.com>
2024-09-14 19:16:19 +0700

    update flake8

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,17 @@
+"""Admin class."""
 from django.contrib import admin
 
 from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
+    """Inline admin descriptor for Choice model."""
     model = Choice
     extra = 3
 
+
 class QuestionAdmin(admin.ModelAdmin):
+    """Admin interface for the Question model."""
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
@@ -16,6 +20,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently', 'end_date', 'can_vote')
     list_filter = ['pub_date', 'end_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/apps.py
+++ polls/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
+    """Configuration for the Polls application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'

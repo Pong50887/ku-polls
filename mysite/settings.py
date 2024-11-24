49e3b3cada716ae9e63013cf4ed2bc2970799c70
Pichayoot Tanasinanan <pichayoot.pong@gmail.com>
2024-11-24 20:25:38 +0700

    Create docker and docker-compose

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "USER": config("DATABASE_USER", default="pollsapp"),
+        "PASSWORD": config("DATABASE_PASSWORD", default="password"),
+        "HOST": config("DATABASE_HOST", default="localhost"),
+        "PORT": config("DATABASE_PORT", default="5432")
     }
 }
 
